8022cffaba30311140c747cc239e43fe3489ba2b
Olena <kryhtopa@gmail.com>
2022-05-12 20:17:58 +0200

    add change

--- src/homework3/Task2.java
+++ src/homework3/Task2.java
@@ -5,10 +5,9 @@ public class Task2 {
         int a = 2;
         int b = 4;
         int c = 8;
-        //| a- b | / (a + b) ^3 - √c
         double results;
         results = (Math.abs(a - b) / Math.pow(a + b, 3) - Math.sqrt(c));
-        System.out.println ("Відповідь: " + results);
+        System.out.println("Відповідь: " + results);
 
     }
 }
