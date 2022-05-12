8f536e7e4576c4babc9657ba9b3e7729e615dbac
Olena <kryhtopa@gmail.com>
2022-05-12 11:48:31 +0200

    add change

--- src/homework2/Location.java
+++ src/homework2/Location.java
@@ -6,8 +6,8 @@ public class Location {
         double longitude = 66.21409;
         double latitude = 13.37714;
         System.out.println( "Мої координати: ");
-        System.out.println( "-довжина " + longitude + "; " );
-        System.out.println( "-ширина  " + latitude + ". " );
+        System.out.println( "-довгота " + longitude + "; " );
+        System.out.println( "-широта  " + latitude + ". " );
 
     }
 }
--- src/homework2/Math.java
+++ src/homework2/Math.java
@@ -6,9 +6,12 @@ public class Math {
 
         int a = 10;
         int b = 20;
-        System.out.println( a = a+b );
-        System.out.println( b = a-b);
-        System.out.println( a = a-b );
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println( "a = " + a );
+        System.out.println( "b = " + b );
 
     }
 
