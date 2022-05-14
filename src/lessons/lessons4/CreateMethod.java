c6f6cb3a2292179a29282bc5cf74813f0083af47
Olena <kryhtopa@gmail.com>
2022-05-14 11:24:40 +0200

    add change

---
+++ src/lessons/lessons4/CreateMethod.java
@@ -0,0 +1,12 @@
+package lessons.lessons4;
+
+public class CreateMethod {
+    public static void main(String[] args) {
+
+    }
+    public static void printMessage() {
+        System.out.println("message");
+
+    }
+
+}
---
+++ src/lessons/lessons4/CreateMethodUsage.java
@@ -0,0 +1,10 @@
+package lessons.lessons4;
+
+public class CreateMethodUsage {
+    public static void main(String[] args) {
+        CreateMethod.printMessage();
+
+    }
+
+}
+
