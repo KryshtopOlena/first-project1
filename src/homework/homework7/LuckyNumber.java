e7336f711d18d2ed94cd84c0f645c274ae38932b
Olena <kryhtopa@gmail.com>
2022-05-24 15:31:10 +0200

    add change

--- src/homework/homework7/LuckyNumber.java
+++ src/homework/homework7/LuckyNumber.java
@@ -3,10 +3,9 @@ package homework.homework7;
 public class LuckyNumber {
 
     public static void main(String[] args) {
-        int i;
         int unlucky1 = 4;
         int unlucky2 = 9;
-        for (i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 100; i++) {
             if (i % 10 == unlucky1 || (i / 10) % 10 == unlucky1 || i % 10 == unlucky2 || (i / 10) % 10 == unlucky2) {
                 continue;
             }
