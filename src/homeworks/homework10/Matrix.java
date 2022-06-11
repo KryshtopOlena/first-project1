5b9c7e14df5aafae9375f2194dd646b3d5c10ff1
Olena <kryhtopa@gmail.com>
2022-06-11 13:34:52 +0200

    add change

--- src/homeworks/homework10/Matrix.java
+++ src/homeworks/homework10/Matrix.java
@@ -35,16 +35,15 @@ public class Matrix {
     }
 
     public static void print(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
             System.out.println();
         }
     }
 
     public static void valueExchange(int[][] matrix, int[][] transposedMatrix) {
-
         for (int i = 0; i < transposedMatrix.length; i++) {
             for (int j = 0; j < transposedMatrix[i].length; j++) {
                 transposedMatrix[i][j] = matrix[j][i];
